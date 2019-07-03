519adba21994c11eb0910c3d107063518cc01b3b
chen <1423379884@qq.com>
2019-07-03 19:46:19 +0800

    add db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+//手动导入Schema类
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 该表字符集长度
+        Schema::defaultStringLength(191);
     }
 
     /**
